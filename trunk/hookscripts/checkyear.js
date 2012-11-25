b715bc3ffed561e9d473de1f6156293dbfab2046
XhmikosR <XhmikosR@98b43a4e-47f0-47d6-ba00-cf671fc20f81>
2012-11-25 15:40:59 +0000

    update checkyear.js

--- trunk/hookscripts/checkyear.js
+++ trunk/hookscripts/checkyear.js
@@ -1,19 +1,20 @@
-// this script is a local pre-commit hook script.
-// Used to check whether the copyright year of modified files has been bumped
-// up to the current (2011) year.
-//
-// Only *.cpp and *.h files are checked
-//
-// Set the local hook scripts like this (pre-commit hook):
-// WScript path/to/this/script/file.js
-// and set "Wait for the script to finish"
+/* This script is a local pre-commit hook script.
+ * It's used to check whether the copyright year of modified files has been
+ * bumped up to the current (2012) year.
+ *
+ * Only *.cpp and *.h files are checked
+ *
+ * Set the local hook scripts like this (pre-commit hook):
+ * WScript path/to/this/script/file.js
+ * and set "Wait for the script to finish"
+ */
 
-var ForReading = 1
-var objArgs,num;
+var ForReading = 1;
+var objArgs, num;
 
 objArgs = WScript.Arguments;
 num = objArgs.length;
-if (num != 4)
+if (num !== 4)
 {
     WScript.Echo("Usage: [CScript | WScript] checkyear.js path/to/pathsfile depth path/to/messagefile path/to/CWD");
     WScript.Quit(1);
@@ -30,24 +31,25 @@ var files = readPaths(objArgs(0));
 // going backwards with while is believed to be faster
 var fileindex = files.length;
 var errormsg = "";
+
 while (fileindex--)
 {
     var f = files[fileindex];
-    if (f.match(filere) != null)
+    if (f.match(filere) !== null)
     {
         if (fs.FileExists(f))
         {
             a = fs.OpenTextFile(f, ForReading, false);
             var copyrightFound = false;
             var yearFound = false;
-            while ((!a.AtEndOfStream)&&(!yearFound))
+            while ((!a.AtEndOfStream) && (!yearFound))
             {
                 r =  a.ReadLine();
                 rv = r.match(basere);
-                if (rv != null)
+                if (rv !== null)
                 {
                     rv = r.match(re);
-                    if (rv != null)
+                    if (rv !== null)
                         yearFound = true;
 
                     copyrightFound = true;
@@ -57,7 +59,7 @@ while (fileindex--)
 
             if (copyrightFound && (!yearFound))
             {
-                if (errormsg != "")
+                if (errormsg !== "")
                     errormsg += "\n";
                 errormsg += f;
                 found = false;
@@ -66,7 +68,7 @@ while (fileindex--)
     }
 }
 
-if (found == false)
+if (found === false)
 {
     errormsg = "the file(s):\n" + errormsg + "\nhave not the correct copyright year!";
     WScript.stderr.writeLine(errormsg);
