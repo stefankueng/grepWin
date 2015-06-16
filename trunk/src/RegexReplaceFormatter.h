2c0544091ae617b275810df769851efbf483f716
steveking <steveking@98b43a4e-47f0-47d6-ba00-cf671fc20f81>
2015-06-16 19:42:55 +0000

    Add a Formatter for char strings. We need that for replacing strings in big/binary files.

--- trunk/src/RegexReplaceFormatter.h
+++ trunk/src/RegexReplaceFormatter.h
@@ -1,6 +1,6 @@
 // grepWin - regex search and replace for Windows
 
-// Copyright (C) 2011-2012, 2014 - Stefan Kueng
+// Copyright (C) 2011-2012, 2014-2015 - Stefan Kueng
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -24,6 +24,8 @@
 #include <cctype>
 #include <map>
 #include <boost/regex.hpp>
+#include <boost/spirit/include/classic_file_iterator.hpp>
+#include <boost/iostreams/device/mapped_file.hpp>
 
 class NumberReplacer
 {
@@ -44,7 +46,27 @@ public:
     std::wstring    expression;
 };
 
-extern std::vector<NumberReplacer> g_incVec;
+class NumberReplacerA
+{
+public:
+    NumberReplacerA()
+        : leadzero(false)
+        , padding(0)
+        , start(1)
+        , increment(1)
+    {
+
+    }
+
+    bool            leadzero;
+    int             padding;
+    int             start;
+    int             increment;
+    std::string     expression;
+};
+
+extern std::vector<NumberReplacer>  g_incVec;
+extern std::vector<NumberReplacerA> g_incVecA;
 
 class RegexReplaceFormatter
 {
@@ -171,4 +193,130 @@ public:
 private:
     std::wstring            m_sReplace;
     std::map<std::wstring, std::wstring> m_replaceMap;
+};
+
+class RegexReplaceFormatterA
+{
+public:
+    RegexReplaceFormatterA(const std::string &sReplace)
+        : m_sReplace(sReplace)
+    {
+        g_incVec.clear();
+        // parse for ${count0L}, ${count0L(n)}, ${count0L(n,m)}, where
+        // ${count}
+        // is replaced later with numbers starting from 1, incremented by 1
+        // ${count(n)}
+        // is replaced with numbers starting from n, incremented by 1
+        // ${count(n,m)}
+        // is replaced with numbers starting from n, incremented by m
+        // 0 and L are optional and specify the size of the right-aligned
+        // number string. If 0 is specified, zeros are used for padding, otherwise spaces.
+        //boost::wregex expression = boost::wregex(L"(?<!\\\\)\\$\\{count(?<leadzero>0)?(?<length>\\d+)?(\\((?<startval>[-0-9]+)\\)||\\((?<startval>[-0-9]+),(?<increment>[-0-9]+)\\))?\\}", boost::regex::normal);
+        boost::regex expression = boost::regex("\\$\\{count(?<leadzero>0)?(?<length>\\d+)?(\\((?<startval>[-0-9]+)\\)||\\((?<startval>[-0-9]+),(?<increment>[-0-9]+)\\))?\\}", boost::regex::normal);
+        boost::match_results<std::string::const_iterator> whatc;
+        std::string::const_iterator start, end;
+        start = m_sReplace.begin();
+        end = m_sReplace.end();
+        boost::match_flag_type flags = boost::match_default | boost::format_all;
+        while (boost::regex_search(start, end, whatc, expression, flags))
+        {
+            if (whatc[0].matched)
+            {
+                NumberReplacerA nr;
+                nr.leadzero = (((std::string)whatc["leadzero"]) == "0");
+                nr.padding = atoi(((std::string)whatc["length"]).c_str());
+                std::string s = (std::string)whatc["startval"];
+                if (!s.empty())
+                    nr.start = atoi(s.c_str());
+                s = (std::string)whatc["increment"];
+                if (!s.empty())
+                    nr.increment = atoi(s.c_str());
+                if (nr.increment == 0)
+                    nr.increment = 1;
+                nr.expression = (std::string)whatc[0];
+                g_incVecA.push_back(nr);
+            }
+            // update search position:
+            if (start == whatc[0].second)
+            {
+                if (start == end)
+                    break;
+                ++start;
+            }
+            else
+                start = whatc[0].second;
+            // update flags:
+            flags |= boost::match_prev_avail;
+            flags |= boost::match_not_bob;
+        }
+    }
+
+    void SetReplacePair(const std::string& s1, const std::string& s2)
+    {
+        m_replaceMap[s1] = s2;
+    }
+
+    template<typename IT>
+    std::string operator()(boost::match_results<IT> what) const
+    {
+        std::string sReplace = what.format(m_sReplace);
+        if (!m_replaceMap.empty())
+        {
+            for (auto it = m_replaceMap.cbegin(); it != m_replaceMap.cend(); ++it)
+            {
+                auto it_begin = std::search(sReplace.begin(), sReplace.end(), it->first.begin(), it->first.end());
+                while (it_begin != sReplace.end())
+                {
+                    if ((it_begin == sReplace.begin()) || ((*(it_begin - 1)) != '\\'))
+                    {
+                        auto it_end = it_begin + it->first.size();
+                        sReplace.replace(it_begin, it_end, it->second);
+                    }
+                    else if ((*(it_begin - 1)) == '\\')
+                    {
+                        sReplace.erase(it_begin - 1);
+                    };
+                    it_begin = std::search(sReplace.begin(), sReplace.end(), it->first.begin(), it->first.end());
+                }
+            }
+        }
+        if (!g_incVec.empty())
+        {
+            for (auto it = g_incVec.begin(); it != g_incVec.end(); ++it)
+            {
+                auto it_begin = std::search(sReplace.begin(), sReplace.end(), it->expression.begin(), it->expression.end());
+                if (it_begin != sReplace.end())
+                {
+                    if ((it_begin == sReplace.begin()) || ((*(it_begin - 1)) != '\\'))
+                    {
+                        auto it_end = it_begin + it->expression.size();
+                        char format[10] = { 0 };
+                        if (it->padding)
+                        {
+                            if (it->leadzero)
+                                sprintf_s(format, _countof(format), "%%0%dd", it->padding);
+                            else
+                                sprintf_s(format, _countof(format), "%%%dd", it->padding);
+                        }
+                        else
+                            strcpy_s(format, "%d");
+                        char buf[50] = { 0 };
+                        sprintf_s(buf, _countof(buf), format, it->start);
+                        sReplace.replace(it_begin, it_end, buf);
+                        it->start += it->increment;
+                    }
+                    else if ((*(it_begin - 1)) == '\\')
+                    {
+                        sReplace.erase(it_begin - 1);
+                    };
+                }
+            }
+        }
+
+        return sReplace;
+    }
+
+private:
+    std::string             m_sReplace;
+    std::map<std::string, std::string> m_replaceMap;
 };
