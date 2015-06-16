4fe90630e00465eb46cf0ab77ab65e45bf996758
Stefan Küng <stefankueng@gmail.com>
2015-06-16 19:42:55 +0000

    Add a Formatter for char strings. We need that for replacing strings in big/binary files.

--- src/RegexReplaceFormatter.cpp
+++ src/RegexReplaceFormatter.cpp
@@ -1,6 +1,6 @@
 // grepWin - regex search and replace for Windows
 
-// Copyright (C) 2011 - Stefan Kueng
+// Copyright (C) 2011, 2015 - Stefan Kueng
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -19,4 +19,5 @@
 #include "stdafx.h"
 #include "RegexReplaceFormatter.h"
 
-std::vector<NumberReplacer> g_incVec;
+std::vector<NumberReplacer>  g_incVec;
+std::vector<NumberReplacerA> g_incVecA;
